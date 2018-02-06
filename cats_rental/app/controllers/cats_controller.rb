6fb597364581e8fb01d8f4d22227e003831e5124
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-06 05:13:22 -0800

    Created new form

--- cats_rental/app/controllers/cats_controller.rb
+++ cats_rental/app/controllers/cats_controller.rb
@@ -1,4 +1,5 @@
 class CatsController < ApplicationController
+
   def index
     @cats = Cat.all
     render :index
@@ -11,5 +12,23 @@ class CatsController < ApplicationController
 
   def new
     @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      flash[:msg] = "Cat info is successfully saved!"
+      redirect_to cats_url
+    else
+      @cat.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+
+  def cat_params
+    params.require(:cat).permit(:age, :birth_date, :color, :description, :name, :sex)
   end
 end
--- cats_rental/app/models/cat.rb
+++ cats_rental/app/models/cat.rb
@@ -2,10 +2,10 @@ require 'action_view'
 include ActionView::Helpers::DateHelper
 class Cat < ApplicationRecord
 
-  CAT_COLORS = %w(red, white, grey, pink, orange, black).freeze
+  CAT_COLORS = %w(red white grey pink orange black).freeze
 
   validates :color, inclusion: CAT_COLORS
-  validates :sex, inclusion: %w(M,F)
+  validates :sex, inclusion: %w(M F)
 
   def age
     time_ago_in_words(birth_date)
