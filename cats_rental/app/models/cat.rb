31744bb3092533d0ec0c405ec05c04aba4a6d8e6
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-05 22:31:57 -0800

    Removed Pry, Finished 'cat.rb' and its validaton, created age method

---
+++ cats_rental/app/models/cat.rb
@@ -0,0 +1,12 @@
+class Cat < ApplicationRecord
+
+  CAT_COLORS = %w(red, white, grey, pink, orange, black).freeze
+
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: %w(M,F)
+
+  def age
+    time_ago_in_words(birth_date)
+  end
+
+end
