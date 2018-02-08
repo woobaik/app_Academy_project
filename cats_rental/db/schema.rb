553d56f1476d68ee9da71a7d2c0006a142f283e1
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-07 22:27:57 -0800

    Created a migration file, and migrated it to schema

--- cats_rental/db/schema.rb
+++ cats_rental/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206051401) do
+ActiveRecord::Schema.define(version: 20180208061900) do
 
   create_table "cats", force: :cascade do |t|
     t.date "birth_date", null: false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20180206051401) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
