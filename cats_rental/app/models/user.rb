a48ca536a267a18f610f6f69c9ac60115ed5e341
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-07 23:14:49 -0800

    Added Password setter method for 'user.rb'

--- cats_rental/app/models/user.rb
+++ cats_rental/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
