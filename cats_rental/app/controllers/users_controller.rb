5d14afff809e1b2be5cc0dc6cc29348f7110fd54
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-02-07 23:49:04 -0800

    Created user model

---
+++ cats_rental/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
